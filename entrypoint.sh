60642312c4108f4b0e6203ca82597bfd531c6c98
Jae <jae.shok@sendbird.com>
2020-03-08 15:46:00 -0700

    another test

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,9 @@
 
 echo "Hello $1"
 time=$(date)
+IFS='.' # space is set as delimiter
+read -ra ADDR <<< "$1" # str is read into an array as tokens separated by IFS
+for i in "${ADDR[@]}"; do # access each element of array
+    echo "$i"
+done
 echo ::set-output name=time::$time
